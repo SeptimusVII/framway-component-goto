e949e48c097de0827ac696183d95d2a28839fede
Quentin Vansteene <vansteene.quentin@gmail.com>
2020-09-24 16:33:36 +0200

    add css, js and html sample

--- goto.js
+++ goto.js
@@ -8,8 +8,29 @@ module.exports = function(app){
     // Goto.loadingMsg     = "This message will display in the console when component will be loaded.";
     // Goto.requires       = [];
 
-    // Goto.prototype.onCreate = function(){
-    // do thing after element's creation
-    // }
+    Goto.prototype.onCreate = function(){
+        var goto = this;
+        goto.$el.on('click',function(e){
+            e.preventDefault();
+            var target = $(this).data('goto').split(',');
+            if (target.length == 1) target = target[0];else {
+                $.each(target, function (index, item) {
+                    if ($('#' + item).length) {
+                        target = item;
+                        return false;
+                    }
+                });
+            }
+            if ($('#' + target).length) {
+                var scrollValue = $('#' + target).offset().top - ($('#header').outerHeight() || 0);
+                $('body,html').animate({
+                    scrollTop: scrollValue
+                }, 600).promise().then(function(){
+                // window.location.hash = '#' + target;
+                // window.location.replace(window.location);
+                });
+            }
+        });
+    }
     return Goto;
 }
